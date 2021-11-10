79dd94f5b4da81520e58974e8190276ebadee1ae
Michael Nieves <michael.j.nieves5@gmail.com>
2021-11-10 10:44:42 -0600

    Finished user model

--- LifeSignsBackEnd/src/main/java/com/example/model/User.java
+++ LifeSignsBackEnd/src/main/java/com/example/model/User.java
@@ -29,6 +29,69 @@ private String password;
 @Column(name="email")
 private String email;
 
+public User() {
+	
+}
 
+public User(int userID, int roleID, String username, String password, String email) {
+	super();
+	this.userID = userID;
+	this.roleID = roleID;
+	this.username = username;
+	this.password = password;
+	this.email = email;
+}
+
+public User(String username, String password) {
+	super();
+	this.username = username;
+	this.password = password;
+}
+
+public int getUserID() {
+	return userID;
+}
+
+public void setUserID(int userID) {
+	this.userID = userID;
+}
+
+public int getRoleID() {
+	return roleID;
+}
+
+public void setRoleID(int roleID) {
+	this.roleID = roleID;
+}
+
+public String getUsername() {
+	return username;
+}
+
+public void setUsername(String username) {
+	this.username = username;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+@Override
+public String toString() {
+	return "User [userID=" + userID + ", roleID=" + roleID + ", username=" + username + ", password=" + password
+			+ ", email=" + email + "]";
+}
 
 }
