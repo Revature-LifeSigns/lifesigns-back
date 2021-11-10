38d78ee4528650da7781af2dfab876909607521d
Michael Nieves <michael.j.nieves5@gmail.com>
2021-11-10 15:26:53 -0600

    User repo tweak

--- LifeSignsBackEnd/src/main/java/com/example/repository/UserRepository.java
+++ LifeSignsBackEnd/src/main/java/com/example/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.example.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 	
 	public List<User> findAll();
