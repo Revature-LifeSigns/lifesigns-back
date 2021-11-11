c324b8672f4f3a6cc122e5ca3ac0fed6a7be2825
Jessica Shi <jessicaaa.shi@gmail.com>
2021-11-11 16:52:29 -0600

    working Spring Boot app

--- LifeSignsBackEnd/src/main/java/com/revature/controller/DoctorController.java
+++ LifeSignsBackEnd/src/main/java/com/revature/controller/DoctorController.java
@@ -1,76 +1,76 @@
-package com.revature.controller;
-
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value="/doctors")
-@CrossOrigin(origins="*")
-public class DoctorController {
-
-	//private DoctorService dServ
-	
-	public DoctorController() {}
-	
-	
-	//@Autowired
-	/*public DoctorController(DoctorService dServ){
-		super();
-		this.dServ=dServ;
-		}*/
-	
-//	@GetMapping("/initial")
-//	public ResponseEntity<List<Doctor>> insertInitialDoctors(){
-//		List<Doctor> dList = new ArrayList<Doctor>(Arrays.asList(new Doctor()))
-//				for(Doctor d: dList) {
-//					dServ.insertDoctor(d);
-//				}
-//		return new ResponseEntity<List<Doctor>>(dServ.getAllDoctors(), HttpStatus.CREATED);
-//	}
-	
-//	@GetMapping()
-//	public ResponseEntity<List<Doctor>> getAllDoctors(){
-//		return new ResponseEntity<List<Doctor>>(dServ.getAllDoctors(), HttpStatus.OK);
-//	}
-	
-//	@GetMapping("/{lastname}")
-//	public ResponseEntity<Doctor> getDoctorByLastName(@PathVariable("lastname") String lastname){
-//		Doctor doc = dServ.getDoctorByLastName(lastname);
-//		if(user==null) {
-//			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-//		}
-//		return new ResponseEntity<Doctor>(dServ.getDoctorByFirstName(), HttpStatus.OK);
-//	}
-	
-//	@DeleteMapping("/{lastname}")
-//	public ResponseEntity<String> delteDoctor(@PathVariable("lastname") String lastname){
-//		Doctor doc = dServ.getDoctorByLastName(lastname);
-//		if(doc==null) {
-//			return new ResponseEntity<String>("No User Of That Name: " +lastname, HttpStatus.NOT_FOUND);
-//		}
-//		dServ.deleteDoctor(doc);
-//		return new Response<String>("Doctor: " + lastname + " was deleted", HttpStatus.ACCEPTED);
-//	}
-	
-//	@PostMapping()
-//	public ResponseEntity<Object> insertDoctor(@RequestBody Doctor doc){
-//		System.out.println("Doctor JSON: " + doc);
-//		Doctor newDoc = dServ.insertDoctor(doc);
-//		
-//		if(newDoc == null) {
-//			return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
-//		}
-//		
-//		return new ResponseEntity<>(newDoc, HttpStatus.CREATED);
-//	}
-	
-}
+//package com.revature.controller;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping(value="/doctors")
+//@CrossOrigin(origins="*")
+//public class DoctorController {
+//
+//	//private DoctorService dServ
+//	
+//	public DoctorController() {}
+//	
+//	
+//	//@Autowired
+//	/*public DoctorController(DoctorService dServ){
+//		super();
+//		this.dServ=dServ;
+//		}*/
+//	
+////	@GetMapping("/initial")
+////	public ResponseEntity<List<Doctor>> insertInitialDoctors(){
+////		List<Doctor> dList = new ArrayList<Doctor>(Arrays.asList(new Doctor()))
+////				for(Doctor d: dList) {
+////					dServ.insertDoctor(d);
+////				}
+////		return new ResponseEntity<List<Doctor>>(dServ.getAllDoctors(), HttpStatus.CREATED);
+////	}
+//	
+////	@GetMapping()
+////	public ResponseEntity<List<Doctor>> getAllDoctors(){
+////		return new ResponseEntity<List<Doctor>>(dServ.getAllDoctors(), HttpStatus.OK);
+////	}
+//	
+////	@GetMapping("/{lastname}")
+////	public ResponseEntity<Doctor> getDoctorByLastName(@PathVariable("lastname") String lastname){
+////		Doctor doc = dServ.getDoctorByLastName(lastname);
+////		if(user==null) {
+////			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+////		}
+////		return new ResponseEntity<Doctor>(dServ.getDoctorByFirstName(), HttpStatus.OK);
+////	}
+//	
+////	@DeleteMapping("/{lastname}")
+////	public ResponseEntity<String> delteDoctor(@PathVariable("lastname") String lastname){
+////		Doctor doc = dServ.getDoctorByLastName(lastname);
+////		if(doc==null) {
+////			return new ResponseEntity<String>("No User Of That Name: " +lastname, HttpStatus.NOT_FOUND);
+////		}
+////		dServ.deleteDoctor(doc);
+////		return new Response<String>("Doctor: " + lastname + " was deleted", HttpStatus.ACCEPTED);
+////	}
+//	
+////	@PostMapping()
+////	public ResponseEntity<Object> insertDoctor(@RequestBody Doctor doc){
+////		System.out.println("Doctor JSON: " + doc);
+////		Doctor newDoc = dServ.insertDoctor(doc);
+////		
+////		if(newDoc == null) {
+////			return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
+////		}
+////		
+////		return new ResponseEntity<>(newDoc, HttpStatus.CREATED);
+////	}
+//	
+//}
--- LifeSignsBackEnd/src/main/java/com/revature/controller/NurseController.java
+++ LifeSignsBackEnd/src/main/java/com/revature/controller/NurseController.java
@@ -1,44 +1,44 @@
-package com.revature.controller;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.revature.model.Nurse;
-
-import lombok.NoArgsConstructor;
-
-@RestController
-@RequestMapping(value="/nurse")
-@CrossOrigin(origins="*")
-@NoArgsConstructor
-public class NurseController {
-	
-	@GetMapping("/id/{id}")
-	public ResponseEntity<Object> getNurseByUserId(@PathVariable("id") int user_id) {
-		return new ResponseEntity<>("getNurseByUserId works! user_id = " + user_id, HttpStatus.OK);
-	}
-	
-	@GetMapping("username/{username}")
-	public ResponseEntity<Object> getNurseByUsername(@PathVariable("username") String username) {
-		return new ResponseEntity<>("getNurseByUsername works! username = " + username, HttpStatus.OK);
-	}
-	
-	@PostMapping("/update")
-	public ResponseEntity<Object> updateNurseProfile(@RequestBody Nurse nurse) {
-		return new ResponseEntity<>("updateNurseProfile works! Nurse username = " + nurse.getUserName(), HttpStatus.OK);
-	}
-	
-	//todo: add request body
-	@PostMapping("/diagnosis")
-	public ResponseEntity<Object> sendPatientDiagnosisForm() {
-		return new ResponseEntity<>("sendPatientDiagnosisForm works!", HttpStatus.OK);
-	}
-	
-}
+//package com.revature.controller;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.revature.model.Nurse;
+//
+//import lombok.NoArgsConstructor;
+//
+//@RestController
+//@RequestMapping(value="/nurse")
+//@CrossOrigin(origins="*")
+//@NoArgsConstructor
+//public class NurseController {
+//	
+//	@GetMapping("/id/{id}")
+//	public ResponseEntity<Object> getNurseByUserId(@PathVariable("id") int user_id) {
+//		return new ResponseEntity<>("getNurseByUserId works! user_id = " + user_id, HttpStatus.OK);
+//	}
+//	
+//	@GetMapping("username/{username}")
+//	public ResponseEntity<Object> getNurseByUsername(@PathVariable("username") String username) {
+//		return new ResponseEntity<>("getNurseByUsername works! username = " + username, HttpStatus.OK);
+//	}
+//	
+//	@PostMapping("/update")
+//	public ResponseEntity<Object> updateNurseProfile(@RequestBody Nurse nurse) {
+//		return new ResponseEntity<>("updateNurseProfile works! Nurse username = " + nurse.getUserName(), HttpStatus.OK);
+//	}
+//	
+//	//todo: add request body
+//	@PostMapping("/diagnosis")
+//	public ResponseEntity<Object> sendPatientDiagnosisForm() {
+//		return new ResponseEntity<>("sendPatientDiagnosisForm works!", HttpStatus.OK);
+//	}
+//	
+//}
