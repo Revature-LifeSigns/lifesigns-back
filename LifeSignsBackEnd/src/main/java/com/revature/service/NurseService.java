c853dbcaedc761159c68125da13775b94203b919
gabrielleornella1 <GabrielleOrnella1@yahoo.fr>
2021-11-11 14:23:42 -0600

    add nurseservice class and add some methods in nurse repository

---
+++ LifeSignsBackEnd/src/main/java/com/revature/service/NurseService.java
@@ -0,0 +1,60 @@
+package com.revature.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import com.revature.model.Nurse;
+import com.revature.repositories.NurseRepository;
+
+@Service
+public class NurseService {
+	
+	private NurseRepository nRepo;
+	
+	
+	public NurseService() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public NurseService(NurseRepository nRepo) {
+		super();
+		this.nRepo = nRepo;
+	}
+	
+	public List<Nurse>getAllNurse(){
+		return nRepo.findAll();
+	}
+	
+	public void insertNurse(Nurse nurse) {
+		nRepo.save(nurse);
+	}
+	
+	public Nurse getNurseByUsername(String userName) {
+		return nRepo.findByUserName(userName);
+	}
+	
+	
+	public Nurse getNurseByFirstnameAndLastName(String firstname, String lastname) {
+		return nRepo.findByFirstNameAndLastName(firstname, lastname);
+	}
+	
+	
+	
+	public Nurse getNurseByEmail(String email) {
+		return nRepo.findByEmail(email);
+	}
+	
+	
+	public Nurse getNurseById(int id) {
+		return nRepo.findByUserId(id);
+	}
+	
+	public void UpdateNurse(Nurse nurse) {
+		nRepo.delete(nurse);
+		
+	}
+	
+	
+
+}
