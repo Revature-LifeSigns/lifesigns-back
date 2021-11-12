e65c107ea617b977c316aa3e79fdb8f30291e8c2
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-11-12 16:14:17 -0500

    end of day code push

---
+++ LifeSignsBackEnd/src/main/java/com/revature/config/AmazonConfig.java
@@ -0,0 +1,35 @@
+package com.revature.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class AmazonConfig {
+   
+	ClassLoader cl = getClass().getClassLoader();
+	InputStream is;
+	Properties p= new Properties();
+    
+	public AmazonConfig() {
+		
+	}
+    
+    @Bean
+    public AmazonS3 s3() {
+    	
+        return AmazonS3ClientBuilder
+                .standard()
+                .withRegion("us-east-1")
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .build();
+
+    }
+}
--- LifeSignsBackEnd/src/main/java/com/revature/repository/UserRepository.java
+++ LifeSignsBackEnd/src/main/java/com/revature/repository/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.revature.model.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String>{
+public interface UserRepository extends JpaRepository<User, Integer>{
 	
 	public List<User> findAll();
 	public User findByUsername(String username);
