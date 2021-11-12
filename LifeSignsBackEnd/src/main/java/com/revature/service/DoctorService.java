3fcba588d9ec605023236798ebbf62cdf5c69ae6
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-11-11 19:53:52 -0500

    DoctorService annotation fix

--- LifeSignsBackEnd/src/main/java/com/revature/service/DoctorService.java
+++ LifeSignsBackEnd/src/main/java/com/revature/service/DoctorService.java
@@ -3,10 +3,12 @@ package com.revature.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.revature.model.Doctor;
 import com.revature.repository.DoctorRepository;
 
+@Service
 public class DoctorService {
 	
 	private DoctorRepository doctorRepo;
