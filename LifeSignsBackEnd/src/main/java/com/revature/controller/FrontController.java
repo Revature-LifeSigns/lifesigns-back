1a3c1d223d14f79b0e3b9ff521b86a0409cd2ef5
Amity Seidler <austen.seidler@gmail.com>
2021-11-12 12:52:55 -0600

    added basic controller methods for user and patient chart

--- LifeSignsBackEnd/src/main/java/com/revature/controller/FrontController.java
+++ LifeSignsBackEnd/src/main/java/com/revature/controller/FrontController.java
@@ -1,12 +1,16 @@
 package com.revature.controller;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +18,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.model.PatientChart;
 import com.revature.model.User;
+import com.revature.service.PatientChartService;
 import com.revature.service.UserService;
 import com.revature.util.BcryptPasswordEncoder;
 
@@ -23,12 +29,14 @@ import com.revature.util.BcryptPasswordEncoder;
 @CrossOrigin(origins = "*")
 public class FrontController {
 	private UserService uServ;
+	private PatientChartService pcServ;
 	private PasswordEncoder passwordEncoder;
 	
     @Autowired
-    public FrontController(UserService uServ, BcryptPasswordEncoder BCryptHasher) {
+    public FrontController(UserService uServ, PatientChartService pcServ, BcryptPasswordEncoder BCryptHasher) {
         super();
         this.uServ = uServ;
+        this.pcServ = pcServ;
         this.passwordEncoder = BCryptHasher.getPasswordEncoder();
     }
     
@@ -62,12 +70,136 @@ public class FrontController {
         return new ResponseEntity < > (newUser, HttpStatus.ACCEPTED);
     }
     
-	@GetMapping("/id/{id}")
-	public ResponseEntity<Object> getUserByUserId(@PathVariable("id") int user_id) {
-		return new ResponseEntity<>("getNurseByUserId works! user_id = " + user_id, HttpStatus.OK);
+    //********************
+    //GET Methods
+    //********************
+    
+	@GetMapping("/user")
+	public ResponseEntity<List<User>> getAllUsers() {
+		return new ResponseEntity<List<User>>(uServ.getAllUsers(), HttpStatus.OK);
+	}
+    
+	@GetMapping("/user/id/{id}")
+	public ResponseEntity<User> getUserByUserId(@PathVariable("id") int userid) {
+		User user = uServ.getUserByUserId(userid);
+		if (user == null) {
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(user, HttpStatus.OK);
 	}
 	
+	@GetMapping("/user/username/{username}")
+	public ResponseEntity<User> getUserByUsername(@PathVariable("username") String username) {
+		User user = uServ.getUserByUsername(username);
+		if (user == null) {
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(user, HttpStatus.OK);
+	}
 	
+	@GetMapping("/chart")
+	public ResponseEntity<List<PatientChart>> getAllCharts() {
+		return new ResponseEntity<List<PatientChart>>(pcServ.getAllCharts(), HttpStatus.OK);
+	}
 	
+	@GetMapping("/chart/id/{id}")
+	public ResponseEntity<PatientChart> getChartByChartId(@PathVariable("id") int chartid) {
+		PatientChart chart = pcServ.getChartByChartId(chartid);
+		if (chart == null) {
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(chart, HttpStatus.OK);
+	}
+	
+	//********************
+    //POST Methods
+    //********************
+	
+	@PostMapping("/user/insert")
+	public ResponseEntity<Object> insertUser(@RequestBody User user) {
+		if (uServ.getUserByUserId(user.getUserid()) != null) {
+			return new ResponseEntity<>("User with id " + user.getUserid() + " already exists.", HttpStatus.FORBIDDEN);
+		}
+		String encryptedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encryptedPassword);
+		uServ.insertUser(user);
+		return new ResponseEntity<>(user, HttpStatus.CREATED);
+	}
+	
+	@PostMapping("/user/update")
+	public ResponseEntity<Object> updateUser(@RequestBody User user) {
+		if (uServ.getUserByUserId(user.getUserid()) == null) {
+			return new ResponseEntity<>("User with id " + user.getUserid() + " doesn't exist.", HttpStatus.FORBIDDEN);
+		}
+		uServ.insertUser(user);	
+		return new ResponseEntity<>(user, HttpStatus.ACCEPTED);
+	}
+	
+	@PostMapping("/chart/insert")
+	public ResponseEntity<Object> insertChart(@RequestBody PatientChart chart) {
+		if (pcServ.getChartByChartId(chart.getChartid()) != null) {
+			return new ResponseEntity<>("Chart with id " + chart.getChartid() + " already exists.", HttpStatus.FORBIDDEN);
+		}
+		pcServ.insertChart(chart);
+		return new ResponseEntity<>(chart, HttpStatus.CREATED);
+	}
+	
+	@PostMapping("/chart/update")
+	public ResponseEntity<Object> updateChart(@RequestBody PatientChart chart) {
+		if (pcServ.getChartByChartId(chart.getChartid()) == null) {
+			return new ResponseEntity<>("Chart with id " + chart.getChartid() + " doesn't exist.", HttpStatus.FORBIDDEN);
+		}
+		pcServ.insertChart(chart);
+		return new ResponseEntity<>(chart, HttpStatus.ACCEPTED);
+	}
+	
+	//********************
+    //DELETE Methods
+    //********************
+	
+	@DeleteMapping("/user/id/{id}")
+	public ResponseEntity<String> deleteUser(@PathVariable("id") int userid) {
+		if (uServ.getUserByUserId(userid) == null) {
+			return new ResponseEntity<>("User with id " + userid + " doesn't exist.", HttpStatus.FORBIDDEN);
+		}
+		uServ.deleteUser(uServ.getUserByUserId(userid));
+		return new ResponseEntity<>("User with id " + userid + " deleted.", HttpStatus.ACCEPTED);
+	}
+	
+	@DeleteMapping("/chart/id/{id}")
+	public ResponseEntity<String> deleteChart(@PathVariable("id") int chartid) {
+		if (pcServ.getChartByChartId(chartid) == null) {
+			return new ResponseEntity<>("Chart with id " + chartid + " doesn't exist.", HttpStatus.FORBIDDEN);
+		}
+		pcServ.deleteChart(pcServ.getChartByChartId(chartid));
+		return new ResponseEntity<>("Chart with id " + chartid + " deleted.", HttpStatus.ACCEPTED);
+	}
+	
+	//********************
+    //for testing purposes
+    //********************
+	
+//	@GetMapping("/user/initial")
+//	public ResponseEntity<List<User>> insertInitialUsers() {
+//		List<User> userList = new ArrayList<>();
+//		userList.add(new User("nurse", "SuperNurseSusan", "", "susansusans@gmail.com", "Susan", "Susans", LocalDate.now(), true));
+//		userList.add(new User("doctor", "SuperDoctorSteven", "", "stevenstevens@gmail.com", "Steven", "Stevens", LocalDate.now(), false));
+//		for (User user : userList) {
+//			uServ.insertUser(user);
+//		}
+//		return new ResponseEntity<>(userList, HttpStatus.CREATED);
+//	}
+//	
+//	@GetMapping("chart/initial")
+//	public ResponseEntity<List<PatientChart>> insertInitialCharts() {
+//		User nurse = new User("nurse", "SuperNurseSusan", "", "susansusans@gmail.com", "Susan", "Susans", LocalDate.now(), true);
+//		User doctor = new User("doctor", "SuperDoctorSteven", "", "stevenstevens@gmail.com", "Steven", "Stevens", LocalDate.now(), false);
+//		List<PatientChart> chartList = new ArrayList<>();
+//		chartList.add(new PatientChart(doctor, nurse, "Guy", "Fieri", "guyfieri@flavortown.com", LocalDate.now(), "001 Flavortown Rd.", "", "these are notes", "this is a diagnosis", true, "this is the treatment"));
+//		for (PatientChart chart : chartList) {
+//			pcServ.insertChart(chart);
+//		}
+//		return new ResponseEntity<>(chartList, HttpStatus.CREATED);
+//	}
 
 }
