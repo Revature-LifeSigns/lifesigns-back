44b7442a6559ea13eca055a9a1fc9e33c9122eaf
Amity Seidler <austen.seidler@gmail.com>
2021-11-17 10:14:12 -0600

    made user controller tests and slight edit to user service

--- LifeSignsBackEnd/src/test/java/com/revature/test/UserControllerTests.java
+++ LifeSignsBackEnd/src/test/java/com/revature/test/UserControllerTests.java
@@ -8,6 +8,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,18 +19,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.model.User;
 import com.revature.service.UserService;
-import com.revature.util.BcryptPasswordEncoder;
 
 @SpringBootTest
 public class UserControllerTests {
 
-private MockMvc mockMvc;
+	private MockMvc mockMvc;
 	
 	@Autowired
 	private WebApplicationContext context;
@@ -48,10 +47,38 @@ private MockMvc mockMvc;
 	public void setUp() throws Exception{
 		this.mockMvc = webAppContextSetup(context).build();
 		
+		LocalDate date = LocalDate.now();
 		expectedInputJSON.put("userid", 1);
+		expectedInputJSON.put("role", "nurse");
 		expectedInputJSON.put("username", "user1");
+		expectedInputJSON.put("password", "p4ssw0rd");
+		expectedInputJSON.put("email", "user1@email.com");
+		expectedInputJSON.put("firstName", "user");
+		expectedInputJSON.put("lastName", "one");
+		expectedInputJSON.put("dob", date.toString());
+		expectedInputJSON.put("address", "123 Main St.");
+		expectedInputJSON.put("profile_image", null);
+		expectedInputJSON.put("specialty", "surgery");
+		expectedInputJSON.put("aboutMe", "hello");
+		expectedInputJSON.put("viewPreference", true);
+		expectedInputJSON.put("covidStatus", "none");
 		
-		databaseUser = new User(1, null, "user1", null, null, null, null, null, null, null, null, null, null);
+		
+		databaseUser = new User();
+		databaseUser.setUserid(1);
+		databaseUser.setRole("nurse");
+		databaseUser.setUsername("user1");
+		databaseUser.setPassword("p4ssw0rd");
+		databaseUser.setEmail("user1@email.com");
+		databaseUser.setFirstName("user");
+		databaseUser.setLastName("one");
+		databaseUser.setDob(date);
+		databaseUser.setAddress("123 Main St.");
+		databaseUser.setProfile_image(null);
+		databaseUser.setSpecialty("surgery");
+		databaseUser.setAboutMe("hello");
+		databaseUser.setViewPreference(true);
+		databaseUser.setCovidStatus("none");
 	}
     
 	@Test
@@ -98,6 +125,35 @@ private MockMvc mockMvc;
 	@Test
 	public void ensureGetUserByUsernamePostDoesntExist() throws Exception {
 		this.mockMvc.perform(post("/LifeSigns/user/username/user1"))
+			.andExpect(status().isMethodNotAllowed());
+	}
+	
+	@Test
+	public void insertUserReturnForbiddenIfUserExists() throws Exception {
+		when(uServ.getUserByUserId(1)).thenReturn(databaseUser);
+		this.mockMvc.perform(post("/LifeSigns/user/insert")
+			.content(asJSONString(expectedInputJSON))
+			.contentType(MediaType.APPLICATION_JSON)
+			.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isForbidden());
+	}
+	
+	@Test
+	public void insertUserReturnCreatedIfUserDoesntExist() throws Exception {
+		when(uServ.getUserByUserId(1)).thenReturn(null);
+		when(uServ.getUserByUsername("user1")).thenReturn(null);
+		when(uServ.getUserByEmail("user1@email.com")).thenReturn(null);
+		this.mockMvc.perform(post("/LifeSigns/user/insert")
+			.content(asJSONString(expectedInputJSON))
+			.contentType(MediaType.APPLICATION_JSON)
+			.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("$.username").value("user1"));
+	}
+	
+	@Test
+	public void ensureInsertUserGetDoesntExist() throws Exception {
+		this.mockMvc.perform(get("/LifeSigns/user/insert"))
 			.andExpect(status().isMethodNotAllowed());
 	}
 	
